bc432f8d37ab376c400caaf25566e3e8e40477f6
SakutaTHZ <90145022+Kur0Daisuke@users.noreply.github.com>
2022-11-17 17:35:07 +0630

    updated scripts
    - fixed things those aren't supposed to exist

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from tkinter import *
 from tkinter import filedialog
+from tkinter import messagebox
 from PIL import ImageTk,Image
 import PIL.Image
 import PIL.ImageTk
@@ -7,7 +8,10 @@ import PIL.ImageTk
 def choose():
     folder_selected = filedialog.askdirectory()
     labelVar.set(folder_selected)
-    Button(can,text="create", command= lambda: createFiles(folder_selected), height=1, width=15, bd=1).place(x=140,y=100)
+    if folder_selected != "":
+        createButton.configure(command=lambda: [createFiles(folder_selected), messagebox.showinfo("Success", "Successfully generated!!")])
+    else:
+        createButton.configure(command=lambda: messagebox.showerror("Error", "Warning : \n Reason : No Files Selected"))
 
 def createFiles(src):
     html = open(src + "\index.html", "w")
@@ -22,20 +26,13 @@ def createFiles(src):
     js.write("//have fun coding")
     js.close()
 
+# app 
 root=Tk()
 root.title("Html Generator")
 root.iconbitmap('coffee.ico')
 root.geometry("300x200")
 can = Canvas(root, width=300,height=200)
 can.place(relx=0.5, rely=0.5, anchor=CENTER)
-
-global entry
-
-labelVar = StringVar()
-Label(can, text="Html Project Generator",font=("Arial", 15)).place(x=30,y=30)
-dirs = Label(can, text="Selected Folder", textvariable=labelVar).place(x=30,y=70)
-
-Button(can,text="Choose Folder", command=choose, height=1, width=15, bd=1).place(x=30,y=100)
 
 # Read the Image
 image = PIL.Image.open("coffee.png")
@@ -47,6 +44,17 @@ img = PIL.ImageTk.PhotoImage(resize_image)
  
 # create label and add resize image
 Label(can,image=img).place(relx=0.8, rely=0.7, anchor=CENTER)
+
+createButton = Button(can,text="create", command=lambda: messagebox.showerror("Error", "Warning : \n Reason : No Files Selected"), height=1, width=15, bd=1)
+createButton.place(x=140,y=100)
+
+global entry
+
+labelVar = StringVar()
+Label(can, text="Html Project Generator",font=("Arial", 15)).place(x=30,y=30)
+dirs = Label(can, text="Selected Folder", textvariable=labelVar).place(x=30,y=70)
+
+Button(can,text="Choose Folder", command=lambda: choose(), height=1, width=15, bd=1).place(x=30,y=100)
 
 root.mainloop()
 
